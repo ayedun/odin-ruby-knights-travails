e51a9b8ded048d0352ac186c378626fafbfa1353
ayedun <88404572+ayedun@users.noreply.github.com>
2023-02-13 10:06:01 -0800

    Add knight_moves

--- game_board.rb
+++ game_board.rb
@@ -12,23 +12,22 @@ class Board
     end
 
     def knight_moves(current_node, end_position, queue = Queue.new())
-        # p current_node
-        p current_node.position
-        p "current node position #{current_node.position} and end position #{end_position}}"
         if current_node.position == end_position
-            p "found end position"
+            path = print_path(current_node)
+            # p "found end position"
+            p "The shortest path, made up of #{(path.length-1)} steps to follow is: #{path}"
             return
         end
-        return if current_node.children.empty?
-        current_node.children.each do |child|
-            p "adding child to queue"
-            queue.enq(child)
-            p queue.size
+        if !(current_node.children.empty?)
+            current_node.children.each do |child|
+                # p "adding child to queue"
+                queue.enq(child)
+                # p queue.size
 
+            end
         end
         
-        knight_moves(queue.deq, end_position, queue) unless (queue.empty?)
-        
+        knight_moves(queue.deq, end_position, queue) unless ((queue.empty?) || current_node.position == end_position)
         
         if queue.empty?
             
@@ -36,14 +35,26 @@ class Board
         end
     end
 
+    def print_path(current_node, path = [])
+        # p current_node.position
+        # p current_node.parent.position
+        # p current_node.parent.parent.position
+        # p current_node.parent.parent.parent.position
+        # p current_node.parent.parent.parent.parent
+
+        path.unshift(current_node.position)
+        print_path(current_node.parent, path) unless current_node.parent == nil
+        path
+    end
+
     def print_knight(current_node, queue = Queue.new())
         
-        p "Parent position: #{current_node.position}"
+        # p "Parent position: #{current_node.position}"
         current_node.children.each do |child|
-            puts " \tChild position: #{child.position}"
+            # puts " \tChild position: #{child.position}"
             queue.enq(child)
         end
-        p"moving on to next parent"
+        # p"moving on to next parent"
         print_knight(queue.deq, queue) unless (queue.empty?)
     
     end
--- main.rb
+++ main.rb
@@ -3,18 +3,9 @@ require_relative "./game_board.rb"
 require "pry-byebug"
 
 game = Board.new()
-knight = Knight.new([0,0])
-# game.print_used_positions(knight.return_used_positions) #knight's all used positions
-p ""
-p knight.children.length
-# knight.children.each do |child|
-#     p child.position
-# end
-
+knight = Knight.new([3, 3])
 knight.possible_moves
-#for each children, do possible moves
-#continue until @@used_positions = @@used_positions_max
 game.print_knight(knight)
-p knight.return_used_positions.length
-#game.knight_moves(knight, [3, 0]) #knight's moves from start to end position
+# p knight.return_used_positions
+game.knight_moves(knight, [4, 3]) #knight's moves from start to end position
 #left on creating knight_moves
